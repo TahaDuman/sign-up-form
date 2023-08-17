d070a1513bf3a4c53bf794bf99139568066b1d81
Taha Duman <tahaduman360@gmail.com>
2023-08-17 19:07:12 +0300

    password match validation now works

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const password_2 = document.getElementById("password_2")
 
 password_1.addEventListener("input", event =>{
     let firstPass = password_1.value
-    console.log(firstPass)
+    console.log(`first password is ${firstPass}`)
     if(password_1.value == password_2.value){
         console.log("passwords match")
     }
@@ -11,15 +11,27 @@ password_1.addEventListener("input", event =>{
 
 password_2.addEventListener("input", event =>{
     let secondPass = password_2.value
-    console.log(secondPass)
+    console.log(`second password is ${secondPass}`)
+
+    // border color changes depending on if password matches
+    function changeColorValid(){
+        password_1.style.cssText = "border: #42b883 3px solid;"
+        password_2.style.cssText = "border: #42b883 3px solid;"
+    }
+    function changeColorInvalid(){
+        password_1.style.cssText = "border: red 3px solid;"
+        password_2.style.cssText = "border: red 3px solid;"
+    }
+
+
     if(password_1.value == password_2.value){
         console.log("passwords match")
-    }
+        changeColorValid()
+        password_1.setCustomValidity("")
+        password_2.setCustomValidity("")
+    } else if(password_1.value != password_2){
+        changeColorInvalid()
+        password_1.setCustomValidity("Passwords don't match")
+        password_2.setCustomValidity("Passwords don't match")
+    } 
 })
-
-function passwMatchCheck(){
-    password_1.addEventListener("input", event =>{
-        let firstPass = password_1.value
-    })
-    
-}
