68738e934fc790e0a8dcf602511de3148266cd99
Taha Duman <tahaduman360@gmail.com>
2023-08-17 19:27:25 +0300

    implemented the password length checker in js

--- index.js
+++ index.js
@@ -15,15 +15,19 @@ password_1.addEventListener("input", event =>{
     let firstPass = password_1.value
     console.log(`first password is ${firstPass}`)
 
-    if(password_1.value == password_2.value && password_1.value.length > 7){
+    if(password_1.value.length < 7){
+        changeColorInvalid()
+        password_1.setCustomValidity("Password must be at least 8 characters")
+        password_2.setCustomValidity("Password must be at least 8 characters")
+    } else if(password_1.value != password_2.value){
+        changeColorInvalid()
+        password_1.setCustomValidity("Passwords don't match")
+        password_2.setCustomValidity("Passwords don't match")
+    } else if(password_1.value == password_2.value && password_1.value.length > 7){
         console.log("passwords match")
         changeColorValid()
         password_1.setCustomValidity("")
         password_2.setCustomValidity("")
-    } else if(password_1.value != password_2){
-        changeColorInvalid()
-        password_1.setCustomValidity("Passwords don't match")
-        password_2.setCustomValidity("Passwords don't match")
     } 
 })
 
@@ -31,15 +35,18 @@ password_2.addEventListener("input", event =>{
     let secondPass = password_2.value
     console.log(`second password is ${secondPass}`)
 
-
-    if(password_1.value == password_2.value ){
+    if(password_1.value.length < 7){
+        changeColorInvalid()
+        password_1.setCustomValidity("Password must be at least 8 characters")
+        password_2.setCustomValidity("Password must be at least 8 characters")
+    } else if(password_1.value != password_2.value){
+        changeColorInvalid()
+        password_1.setCustomValidity("Passwords don't match")
+        password_2.setCustomValidity("Passwords don't match")
+    } else if(password_1.value == password_2.value && password_2.value.length > 7){
         console.log("passwords match")
         changeColorValid()
         password_1.setCustomValidity("")
         password_2.setCustomValidity("")
-    } else if(password_1.value != password_2){
-        changeColorInvalid()
-        password_1.setCustomValidity("Passwords don't match")
-        password_2.setCustomValidity("Passwords don't match")
     } 
 })
